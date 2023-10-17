f1f85772e5ecdd82e6e6295727f353b512a7be4e
Jarmen <cachero.jarmen.bscs2022@gmail.com>
2023-10-17 19:24:08 +0800

    Add project files.

---
+++ MilkTeaPOS/Form1.Designer.vb
@@ -0,0 +1,207 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.settingsBtn = New System.Windows.Forms.PictureBox()
+        Me.analyticsBtn = New System.Windows.Forms.PictureBox()
+        Me.logsBtn = New System.Windows.Forms.PictureBox()
+        Me.prodBtn = New System.Windows.Forms.PictureBox()
+        Me.editBtn = New System.Windows.Forms.PictureBox()
+        Me.buyBtn = New System.Windows.Forms.PictureBox()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Panel1.SuspendLayout()
+        CType(Me.settingsBtn, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.analyticsBtn, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.logsBtn, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.prodBtn, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.editBtn, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.buyBtn, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel2.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.FromArgb(CType(CType(44, Byte), Integer), CType(CType(62, Byte), Integer), CType(CType(80, Byte), Integer))
+        Me.Panel1.Controls.Add(Me.settingsBtn)
+        Me.Panel1.Controls.Add(Me.analyticsBtn)
+        Me.Panel1.Controls.Add(Me.logsBtn)
+        Me.Panel1.Controls.Add(Me.prodBtn)
+        Me.Panel1.Controls.Add(Me.editBtn)
+        Me.Panel1.Controls.Add(Me.buyBtn)
+        Me.Panel1.Controls.Add(Me.Panel2)
+        Me.Panel1.Dock = System.Windows.Forms.DockStyle.Left
+        Me.Panel1.ForeColor = System.Drawing.SystemColors.ActiveBorder
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(83, 575)
+        Me.Panel1.TabIndex = 0
+        '
+        'settingsBtn
+        '
+        Me.settingsBtn.BackColor = System.Drawing.Color.Transparent
+        Me.settingsBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.settingsBtn.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.settingsBtn.Image = Global.MilkTeaPOS.My.Resources.Resources.Settings
+        Me.settingsBtn.Location = New System.Drawing.Point(0, 517)
+        Me.settingsBtn.Margin = New System.Windows.Forms.Padding(3, 20, 3, 20)
+        Me.settingsBtn.Name = "settingsBtn"
+        Me.settingsBtn.Padding = New System.Windows.Forms.Padding(0, 10, 0, 40)
+        Me.settingsBtn.Size = New System.Drawing.Size(83, 50)
+        Me.settingsBtn.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.settingsBtn.TabIndex = 9
+        Me.settingsBtn.TabStop = False
+        '
+        'analyticsBtn
+        '
+        Me.analyticsBtn.BackColor = System.Drawing.Color.Transparent
+        Me.analyticsBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.analyticsBtn.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.analyticsBtn.Image = Global.MilkTeaPOS.My.Resources.Resources.Analytics
+        Me.analyticsBtn.Location = New System.Drawing.Point(-1, 368)
+        Me.analyticsBtn.Margin = New System.Windows.Forms.Padding(3, 20, 3, 20)
+        Me.analyticsBtn.Name = "analyticsBtn"
+        Me.analyticsBtn.Padding = New System.Windows.Forms.Padding(0, 10, 0, 40)
+        Me.analyticsBtn.Size = New System.Drawing.Size(88, 50)
+        Me.analyticsBtn.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.analyticsBtn.TabIndex = 8
+        Me.analyticsBtn.TabStop = False
+        '
+        'logsBtn
+        '
+        Me.logsBtn.BackColor = System.Drawing.Color.Transparent
+        Me.logsBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.logsBtn.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.logsBtn.Image = Global.MilkTeaPOS.My.Resources.Resources.Time_Machine
+        Me.logsBtn.Location = New System.Drawing.Point(0, 312)
+        Me.logsBtn.Margin = New System.Windows.Forms.Padding(3, 20, 3, 20)
+        Me.logsBtn.Name = "logsBtn"
+        Me.logsBtn.Padding = New System.Windows.Forms.Padding(0, 10, 0, 40)
+        Me.logsBtn.Size = New System.Drawing.Size(88, 50)
+        Me.logsBtn.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.logsBtn.TabIndex = 7
+        Me.logsBtn.TabStop = False
+        '
+        'prodBtn
+        '
+        Me.prodBtn.BackColor = System.Drawing.Color.Transparent
+        Me.prodBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.prodBtn.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.prodBtn.Image = Global.MilkTeaPOS.My.Resources.Resources.Clipboard_List
+        Me.prodBtn.Location = New System.Drawing.Point(0, 251)
+        Me.prodBtn.Margin = New System.Windows.Forms.Padding(3, 20, 3, 20)
+        Me.prodBtn.Name = "prodBtn"
+        Me.prodBtn.Padding = New System.Windows.Forms.Padding(0, 10, 0, 40)
+        Me.prodBtn.Size = New System.Drawing.Size(88, 50)
+        Me.prodBtn.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.prodBtn.TabIndex = 6
+        Me.prodBtn.TabStop = False
+        '
+        'editBtn
+        '
+        Me.editBtn.BackColor = System.Drawing.Color.Transparent
+        Me.editBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.editBtn.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.editBtn.Image = Global.MilkTeaPOS.My.Resources.Resources.Edit_Product
+        Me.editBtn.Location = New System.Drawing.Point(0, 194)
+        Me.editBtn.Margin = New System.Windows.Forms.Padding(3, 20, 3, 20)
+        Me.editBtn.Name = "editBtn"
+        Me.editBtn.Padding = New System.Windows.Forms.Padding(0, 10, 0, 40)
+        Me.editBtn.Size = New System.Drawing.Size(88, 50)
+        Me.editBtn.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.editBtn.TabIndex = 5
+        Me.editBtn.TabStop = False
+        '
+        'buyBtn
+        '
+        Me.buyBtn.BackColor = System.Drawing.Color.Transparent
+        Me.buyBtn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.buyBtn.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.buyBtn.Image = Global.MilkTeaPOS.My.Resources.Resources.Basket___Filled
+        Me.buyBtn.Location = New System.Drawing.Point(0, 133)
+        Me.buyBtn.Margin = New System.Windows.Forms.Padding(3, 20, 3, 20)
+        Me.buyBtn.Name = "buyBtn"
+        Me.buyBtn.Padding = New System.Windows.Forms.Padding(0, 10, 0, 40)
+        Me.buyBtn.Size = New System.Drawing.Size(88, 50)
+        Me.buyBtn.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.buyBtn.TabIndex = 4
+        Me.buyBtn.TabStop = False
+        '
+        'Panel2
+        '
+        Me.Panel2.Controls.Add(Me.PictureBox1)
+        Me.Panel2.Dock = System.Windows.Forms.DockStyle.Top
+        Me.Panel2.Location = New System.Drawing.Point(0, 0)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(83, 127)
+        Me.Panel2.TabIndex = 4
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.Color.Transparent
+        Me.PictureBox1.Cursor = System.Windows.Forms.Cursors.Hand
+        Me.PictureBox1.Dock = System.Windows.Forms.DockStyle.Top
+        Me.PictureBox1.Image = Global.MilkTeaPOS.My.Resources.Resources.Milkshake__1_
+        Me.PictureBox1.Location = New System.Drawing.Point(0, 0)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(83, 65)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 3
+        Me.PictureBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.CausesValidation = False
+        Me.ClientSize = New System.Drawing.Size(1031, 575)
+        Me.Controls.Add(Me.Panel1)
+        Me.Name = "Form1"
+        Me.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.Panel1.ResumeLayout(False)
+        CType(Me.settingsBtn, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.analyticsBtn, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.logsBtn, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.prodBtn, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.editBtn, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.buyBtn, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel2.ResumeLayout(False)
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents buyBtn As PictureBox
+    Friend WithEvents analyticsBtn As PictureBox
+    Friend WithEvents logsBtn As PictureBox
+    Friend WithEvents prodBtn As PictureBox
+    Friend WithEvents editBtn As PictureBox
+    Friend WithEvents settingsBtn As PictureBox
+End Class
---
+++ MilkTeaPOS/Form1.vb
@@ -0,0 +1,43 @@
+﻿Public Class Form1
+    Private currentBtn As PictureBox
+    Private Sub ActivateButton(sender As Object)
+        If sender IsNot Nothing Then
+            currentBtn = CType(sender, PictureBox)
+
+            currentBtn.BackColor = Color.FromArgb(64, 84, 104)
+        End If
+    End Sub
+
+    Private Sub DisableButton()
+        If currentBtn IsNot Nothing Then
+            currentBtn.BackColor = Color.FromArgb(44, 62, 80)
+        End If
+    End Sub
+
+    Private Sub buyBtn_Click(sender As Object, e As EventArgs) Handles buyBtn.Click
+        DisableButton()
+        ActivateButton(sender)
+    End Sub
+
+    Private Sub editBtn_Click(sender As Object, e As EventArgs) Handles editBtn.Click
+        DisableButton()
+        ActivateButton(sender)
+    End Sub
+
+    Private Sub prodBtn_Click(sender As Object, e As EventArgs) Handles prodBtn.Click
+        DisableButton()
+        ActivateButton(sender)
+    End Sub
+
+    Private Sub logsBtn_Click(sender As Object, e As EventArgs) Handles logsBtn.Click
+        DisableButton()
+        ActivateButton(sender)
+    End Sub
+
+    Private Sub analyticsBtn_Click(sender As Object, e As EventArgs) Handles analyticsBtn.Click
+        DisableButton()
+        ActivateButton(sender)
+    End Sub
+
+
+End Class
